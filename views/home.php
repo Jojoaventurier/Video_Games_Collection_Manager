bed77453af1a91ada249b51c6f92422a423cea10
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2025-01-10 13:46:59 +0100

    corrected a link on homepage

--- views/home.php
+++ views/home.php
@@ -2,5 +2,5 @@
 <div class="container mx-auto mt-10">
     <h1 class="text-3xl font-bold mb-5">Welcome to Video Game Collection</h1>
     <p class="mb-5">Track your physical and digital games, including platforms, updates, and storage location.</p>
-    <a href="add_game.php" class="btn btn-primary">Add a New Game</a>
+    <a href="index.php?page=add_game" class="btn btn-primary">Add a New Game</a>
 </div>
